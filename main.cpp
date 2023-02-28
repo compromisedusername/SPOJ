abba4ea2526460633a3ea7caf29af76a08982388
compromisedusername <100251433+compromisedusername@users.noreply.github.com>
2023-02-28 20:50:02 +0100

    Delete main.cpp

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "PolePewnegoKola.h"
-int main() {
-    std::cout << "Pole Pewnego Kola";
-    double promien, odleglosc;
-    std::cin >> promien >> odleglosc;
-    PolePewnegoKola::polePewnegoKola(promien, odleglosc);
-}
