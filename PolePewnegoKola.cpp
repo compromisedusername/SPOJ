b2deb5c4e363d609265579e6a5ea530f2df33bd5
compromisedusername <100251433+compromisedusername@users.noreply.github.com>
2023-02-28 20:50:30 +0100

    Delete PolePewnegoKola.cpp

--- PolePewnegoKola.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by bartek on 2/28/23.
-//
-#include <iostream>
-#include "PolePewnegoKola.h"
-void PolePewnegoKola::polePewnegoKola(double r, double d){
-    d/=2;
-    std::cout << r*r*3.141592654 - d*d*3.141592654;
-}
