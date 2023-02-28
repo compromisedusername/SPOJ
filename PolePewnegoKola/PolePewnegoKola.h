ba75e0f74f45301e0ce1eced4df338019aa07b4a
compromisedusername <100251433+compromisedusername@users.noreply.github.com>
2023-02-28 20:49:33 +0100

    Add files via upload

---
+++ PolePewnegoKola/PolePewnegoKola.h
@@ -0,0 +1,15 @@
+//
+// Created by bartek on 2/28/23.
+//
+
+#ifndef UNTITLED4_POLEPEWNEGOKOLA_H
+#define UNTITLED4_POLEPEWNEGOKOLA_H
+
+
+class PolePewnegoKola {
+public:
+    static void polePewnegoKola(double r, double d);
+};
+
+
+#endif //UNTITLED4_POLEPEWNEGOKOLA_H
---
+++ PolePewnegoKola/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "PolePewnegoKola.h"
+int main() {
+    double promien, odleglosc;
+    std::cin >> promien >> odleglosc;
+    PolePewnegoKola::polePewnegoKola(promien, odleglosc);
+}
